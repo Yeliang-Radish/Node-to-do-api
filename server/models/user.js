51c5c8da6cc77c72f45b2eaac890b539588ea040
Yeliang Shou <yeliang.shou@gmail.com>
2018-07-18 22:57:54 -0400

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 
+
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -66,6 +67,25 @@ UserSchema.statics.findByToken = function (token) {
         '_id': decoded._id,
         'tokens.token': token,
         'tokens.access': 'auth'
+    });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
     });
 };
 
